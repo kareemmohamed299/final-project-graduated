d3ab03b378ca3b43450741f756c73b630f0491f1
kareemmohamed299 <kareemmohmed299@gmail.com>
2020-04-03 03:46:21 +0200

    last update

--- app/src/main/java/com/example/project1/connection/Api.java
+++ app/src/main/java/com/example/project1/connection/Api.java
@@ -4,11 +4,15 @@ import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface Api {
 
     @POST("api/student")
 
     public Call<student> getdata(@Body HashMap<Object, Object> map);
+    @GET("api/course")
+    public Call<cource> courcedata(@Query("code")String code);
 }
--- app/src/main/java/com/example/project1/course_info.java
+++ app/src/main/java/com/example/project1/course_info.java
@@ -3,12 +3,29 @@ package com.example.project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class course_info extends AppCompatActivity {
-
+    ArrayList<String> c;
+    private TextView course,professor,Duration,Exam_Start,Exam_End,Degree;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.course_info);
+        c = getIntent().getStringArrayListExtra("c");
+        course=(TextView)findViewById(R.id.t1);
+        professor=(TextView)findViewById(R.id.t2);
+        Duration=(TextView)findViewById(R.id.t3);
+        Exam_Start=(TextView)findViewById(R.id.t4);
+        Exam_End=(TextView)findViewById(R.id.t5);
+        Degree=(TextView)findViewById(R.id.t6);
+        course.setText(c.get(4));
+        professor.setText(c.get(5));
+        Duration.setText(c.get(0));
+        Exam_Start.setText(c.get(1));
+        Exam_End.setText(c.get(2));
+        Degree.setText(c.get(3));
     }
 }
